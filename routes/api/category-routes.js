7decfbebd81558f1a393cce7303c63ed6f6a29bd
Peter Yockey <peterjyockey@gmail.com>
2023-05-09 19:19:22 -0500

    added get routes for tag-routes.js, fixed problems with product and category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,10 +3,17 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  Category.findAll().then((categoryData) => {
-    res.json(categoryData);
-  });
+router.get('/', async (req, res) => {
+  // find all categories
+  // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', async (req, res) => {
